d69aeb8258f1b1796e268b6932e54f137d411dbb
Jan Van den Enden <jan@enden.be>
2020-01-20 14:46:16 +0100

    new build

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -6,7 +6,6 @@ const preferDefault = m => m && m.default || m
 
 exports.components = {
   "component---src-templates-project-post-js": hot(preferDefault(require("/Users/janvandenenden/Desktop/web design and programming/laura-van-haecke/src/templates/project-post.js"))),
-  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/janvandenenden/Desktop/web design and programming/laura-van-haecke/.cache/dev-404-page.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("/Users/janvandenenden/Desktop/web design and programming/laura-van-haecke/src/pages/index.js"))),
   "component---src-pages-projects-js": hot(preferDefault(require("/Users/janvandenenden/Desktop/web design and programming/laura-van-haecke/src/pages/projects.js")))
 }
---
+++ public/component---src-pages-projects-js-ff5d00ab1dd9624ee6ce.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{"16l3":function(e,a,n){"use strict";n.r(a);var t=n("q1tI"),c=n.n(t),l=n("7oih"),r=n("7vrA"),s=n("QTGF"),o=n("JFzz"),u=n.n(o);n("sg+I");a.default=function(){return c.a.createElement(l.a,null,c.a.createElement("div",{className:"projects"},c.a.createElement(r.a,{className:u.a.content+" projects"},c.a.createElement(s.a,null))))}}}]);
+//# sourceMappingURL=component---src-pages-projects-js-ff5d00ab1dd9624ee6ce.js.map

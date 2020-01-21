ce82d85760290fb567dc677bb311f4d8e59b10d2
Jan Van den Enden <jan@enden.be>
2020-01-21 09:40:07 +0100

    add social media icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,24 +1,30 @@
-import React from 'react'
-import { graphql, useStaticQuery } from 'gatsby'
-import footerStyles from "../styles/footer.module.scss"
-
+import React from "react";
+import { graphql, useStaticQuery } from "gatsby";
+import footerStyles from "../styles/footer.module.scss";
+import { FaInstagram, FaVimeo, FaImdb } from "react-icons/fa";
 
 const Footer = () => {
-    const data = useStaticQuery(graphql`
+  const data = useStaticQuery(graphql`
     query {
-        site {
-          siteMetadata {
-            author
-          }
+      site {
+        siteMetadata {
+          author
         }
       }
-    `
-)
-    return(
-        <footer className={footerStyles.footer} >
-            <p>&copy; {data.site.siteMetadata.author}</p>
-        </footer>
-    )
-}
+    }
+  `);
+  return (
+    <footer className={footerStyles.footer}>
+      <div className={footerStyles.socmedia}>
+        <a href="" target="_blank"> <FaVimeo /></a>
+        <a href="" target="_blank">  <FaImdb /></a>
+        <a href="" target="_blank">  <FaInstagram /></a>
+      </div>
+      <a href="https://www.w3schools.com" target="_blank">
+        &copy; 2020
+      </a>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
